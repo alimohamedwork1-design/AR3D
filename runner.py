4506a2bf68fa7df57dd112f24f70a2b42795defe
alimohamedwork1-design <alimohamedwork1@gmail.com>
2026-04-04 04:01:39 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,22 +1,16 @@
-import subprocess
+from supabase import create_client
 import os
 
-# فولدرات
-job_dir = "/tmp/job_ad6f2302-0dd2-428b-bc6b-97cb1a51bd5d"
-image_path = os.path.join(job_dir, "input/images")
-db_path = os.path.join(job_dir, "colmap.db")
+supabase_url = "https://ecypnsmehlznwbfongxt.supabase.co"
+supabase_key = "<sb_secret_xp8VMw91Nw_utNTE0OGCQQ_fjmhQejs>"  # استخدم SERVICE KEY للPod
+supabase = create_client(supabase_url, supabase_key)
 
-os.makedirs(image_path, exist_ok=True)
+os.makedirs("input/images", exist_ok=True)
 
-# تشغيل COLMAP
-try:
-    subprocess.run([
-        "colmap", "feature_extractor",
-        "--database_path", db_path,
-        "--image_path", image_path,
-        "--ImageReader.single_camera", "1",
-        "--SiftExtraction.use_gpu", "0"  # خليها 0 لو GPU مش جاهز
-    ], check=True)
-    print("COLMAP finished successfully!")
-except subprocess.CalledProcessError as e:
-    print("COLMAP crashed!", e)
+# جلب كل الملفات من bucket
+bucket_files = supabase.storage.from_('images').list()
+for f in bucket_files:
+    file_path = f"input/images/{f['name']}"
+    supabase.storage.from_('images').download(f['name'], file_path)
+
+print("All images downloaded from Supabase!")
